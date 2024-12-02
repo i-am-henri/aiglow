ae10cecf4d39977c2f27040192dd6abd6d81acbb
henri <henri.me@skiff.com>
2024-12-02 18:10:06 +0100

    configured better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { admin, organization, username } from "better-auth/plugins";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "sqlite", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    plugins: [
+        admin(),
+        username(),
+        organization(),
+    ]
 });
